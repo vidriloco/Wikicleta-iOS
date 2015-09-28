07219b6d31461757da37829e8981caa31af595df
Alejandro Cruz <vidriloco@gmaikl.com>
2015-09-28 02:01:12 -0500

    Updates for the app to work on ios 9

--- Pods/Pods-SVSegmentedControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SVSegmentedControl : NSObject
-@end
-@implementation PodsDummy_Pods_SVSegmentedControl
-@end
---
+++ Pods/Target Support Files/iActiveRecord/iActiveRecord-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iActiveRecord : NSObject
+@end
+@implementation PodsDummy_iActiveRecord
+@end
